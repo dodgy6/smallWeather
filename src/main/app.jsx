590aafcbcf58916101aa4be6bf47d66733a04fbd
Emanuel Lourenço <dodgy6@gmail.com>
2019-09-04 17:47:52 +0100

    app component - change variable name on map

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -11,14 +11,19 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state      = { weather: '' }
+        this.state      = { 
+            weather: '',
+            min: '',
+            max: '',
+            current: ''
+        }
         this.renderRows = this.renderRows.bind(this)
     }
 
     componentDidMount() {
         axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=Faro,pt&units=metric&APPID=b58080319ae781322bf26cc8a89ac424`).then(res => {
             const weather = res.data.list
-
+            console.log(res)
             this.setState({ weather: weather })
         })
     }
@@ -26,33 +31,35 @@ class App extends Component {
     renderRows(){
         const { weather } = this.state
 
-        return Object.keys(weather).map((w, index) => (
+        return [...weather].map((w, index) => (
             // Filtering first weathers day - 5 days / 3 hours 
-            <If test={index % 8 === 0} key={index}>
-                <div className="row underline">
-                    <div className="col-sm-3 center middle">
-                        <h5>{ weather[index].dt_txt }</h5>
+            <div key={index}>
+                <If test={index % 8 === 0}>
+                    <div className="row underline">
+                        <div className="col-sm-3 center middle">
+                            <h5>{ w.dt_txt }</h5>
+                        </div>
+                        <div className="col-sm-1 center middle">
+                            <h6> {w.weather[0].description} </h6> 
+                        </div>
+                        <div className="col-sm-2 center">
+                            <img src={`http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`} height="100" alt={`weather icon ${w.weather[0].icon}`} />
+                        </div>
+                        <div className="col-sm-2 center">
+                            Min
+                            <h3>{ w.main.temp_min }º</h3>
+                        </div>
+                        <div className="col-sm-2 center">
+                            Current
+                            <h3>{ w.main.temp }º</h3>
+                        </div>
+                        <div className="col-sm-2 center">
+                            Max
+                            <h3>{ w.main.temp_max }º</h3>
+                        </div>
                     </div>
-                    <div className="col-sm-1 center middle">
-                        <h6> {weather[index].weather[0].description} </h6> 
-                    </div>
-                    <div className="col-sm-2 center">
-                        <img src={`http://openweathermap.org/img/wn/${weather[index].weather[0].icon}@2x.png`} height="100" alt={`weather icon ${weather[index].weather[0].icon}`} />
-                    </div>
-                    <div className="col-sm-2 center">
-                        Min
-                        <h3>{ weather[index].main.temp_min }º</h3>
-                    </div>
-                    <div className="col-sm-2 center">
-                        Current
-                        <h3>{ weather[index].main.temp }º</h3>
-                    </div>
-                    <div className="col-sm-2 center">
-                        Max
-                        <h3>{ weather[index].main.temp_max }º</h3>
-                    </div>
-                </div>
-            </If>
+                </If>
+            </div>
         ))
     }
 
